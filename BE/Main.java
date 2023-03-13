c3ed11fe2b08d27fc30304c6f5ecb45f35feb46a
MostafaVT <58429502+MostafaVT@users.noreply.github.com>
2023-03-14 01:52:04 +0200

    final init. of today

--- BE/Main.java
+++
@@ -1,7 +0,0 @@
-package BE;
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hi");
-    }
-    
-}
---
+++ BE/gamequiz/src/main/java/com/quizgame/gamequiz/GamequizApplication.java
@@ -0,0 +1,12 @@
+package com.quizgame.gamequiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamequizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamequizApplication.class, args);
+	}
+}
